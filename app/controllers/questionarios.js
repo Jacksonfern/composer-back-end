7a33c901ff3ba5353be08793a252ae81e0af6419
Jackson Fernandes <jacksonfernandes009@gmail.com>
2018-06-13 17:08:29 -0400

    Adicionando função para pegar as questoes referentes ao questionário e gerar outras versoes, adicionando no banco

--- app/controllers/questionarios.js
+++ app/controllers/questionarios.js
@@ -1,26 +1,66 @@
-/*module.exports.gerarVersoes = function(vet, N){
-    for(i = 1; i <= N; i++){
-        var list = []
-        var aux = vet;
+exports.gerarVersoes = function(params, app){
+    var id_questionario = params.id;
+    var conn = app.config.connection();
+    var questionario = new app.app.models.questionarios(conn);
 
-        while(aux.length > 0){
-            list.push(aux[i % 2]);
-            aux.splice(i % 2, 1);
+    questionario.questions(id_questionario, function(err, result){
+        if(err){
+            console.log("Erro ao se conectar com o banco de dados!" + err.msg);
+            return;
         }
-        console.log(list);
-    }
-}*/
+        var id_questoes = [];
+        for(var i = 0; i < result.length; i++){
+            id_questoes.push(result[i].id);
+        }
+
+        questionario.getLastID(function(err, result){
+            if(err){
+                console.log("Erro ao conectar com o banco de dados! " + err.msg);
+                return;
+            }
+
+            var id_versao = result.id;
+            for(var i = 0; i < N; i++){
+                id_versao++;
+                var ordem = "";
+                var aux = id_questoes.slice();
+                var index = 0;
+                
+                while(aux.length > 0){
+                    index = (index + i) % aux.length;
+                    ordem += aux.splice(index, 1)[0];
+                }
+
+                let versaoQuestionario = {
+                    id_versao_questionario : id_versao,
+                    OA_questao_id_questionario : id_questionario,
+                    ordem : ordem
+                };
+
+                questionario.addVersao(versaoQuestionario, function(err, result){
+                    if(err){
+                        console.log("Erro ao conectar com o banco de dados!");
+                        return;
+                    }
+                });
+            }
+        });
+    });
+}
 
 function gerarVersoes(vet, N){
-    for(var i = 1; i <= N; i++){
-        var list = []
-        var aux = vet;
+	var versoes = [vet];
+	for(var i = 2; i <= N; i++){
+		var list = [];
+		var aux = vet.slice();
+		var index = 0;
 
-        while(aux.length > 0){
-            list.push(aux[i % 2]);
-            aux.splice(i % 2, 1);
-        }
-        console.log(list);
-        return list;
-    }
+		while(aux.length > 0){
+			index = (index + i) % (aux.length);
+      var el = aux.splice(index, 1)[0];
+			list.push(el);
+		}
+		versoes.push(list);
+	}
+	return versoes;
 }
---
+++ app/models/questionarios.js
@@ -0,0 +1,22 @@
+function questionario(connection){
+    this._connection = connection;
+}
+
+questionario.prototype.questions = function(id_questionario, callback){
+    this._connection.query("SELECT OA_questoes_id_questao FROM OA_questionario_id_questionario "
+    + "WHERE OA_questionario_id_questionario = '"+id_questionario+"'", callback);
+}
+
+questionario.prototype.addVersao = function(params, callback){
+    this._connection.query("INSERT INTO versao_questionario SET ?", params,
+        callback);
+}
+
+questionario.prototype.getLastID = function(callback){
+    this._connection.query("SELECT MAX(id_versao_questionario) "
+    + "FROM versao_questionario", callback);
+}
+
+exports = function(){
+    return questionario;
+}
