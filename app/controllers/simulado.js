b015792b5ae6a9ebcacd6a9ba1e19b7fe31534c2
Jackson Fernandes <jacksonfernandes009@gmail.com>
2018-06-23 17:56:21 -0400

    Gerador de versões completo. Falta verificar se o número de versões questionário é igual.

--- app/controllers/simulado.js
+++ app/controllers/simulado.js
@@ -3,46 +3,81 @@ var simuladoModel = require('../models/simulado');
 var conn = connection();
 var simulado = new simuladoModel(conn);
 
-module.exports.getQuestionarios = function(req, res){
+module.exports.createSimulado = function(req, res){ //Adiciona questionarios a um simulado
+    var id_simulado = req.id_simulado; //id para vincular questionarios
+    req.id.forEach(element => {
+        let quest = {
+            simulado_id_simulado : id_simulado,
+            OA_questionario_id_questionario : element
+        }
+        simulado.addQuestionario(quest, function(error, response){
+            if(error){
+                res.send("Erro");
+                return;
+            }
+        });
+    });
+}
+
+module.exports.gerarVersoesSimulados = function(req, res){ //Gera versões de simulados
     var id_simulado = req.id_simulado;
     var id_coordenador = req.id_coordenador;
 
-    simulado.getIDQuestionarios(id_simulado, function(err, result){
+    simulado.getIDQuestionarios(id_simulado, function(err, result){ //Pega os ids dos questionarios vinculados ao simulado
         if(err){
             res.send("Erro");
             return;
         }
 
-        versoesQuestionarios = [];
-        getVersoesQuestionarios(versoesQuestionarios, result);
+        simulado.getLastID(function(err, last_id){
+            if(err){
+                console.log(err);
+                res.send("Erro");
+                return;
+            }
+
+            var id = last_id[0].id_versao_simulado;
+            if(id == null)
+                id = 0;
+            
+            var insert = setVersoesSimulados(id, id_simulado, result, 0);
+            console.log(insert);
+            /**
+             * Função recursiva para vincular as versões de questionários às versões de simulados
+             * OBS: Não consegui fazer sem recursão porque tinha problemas 
+             *      com callbacks e inserção no banco
+             * Apesar de inserir no banco certinho, ele mostra um erro
+             */
+        });
     });
 }
 
-function gerarVersoesSimulados(versoesQuestionarios){
-    simulado.getLastID(function(err, res){
+function setVersoesSimulados(id, id_simulado, questionarios, index){
+    if(index == questionarios.length)
+        return null;
+    
+    simulado.getVersoesQuestionarios(questionarios[index].id_questionario, function(err, result){
         if(err){
+            console.log(err);
             return;
         }
-        
-        id_versao_simulado = res[0].id_versao_simulado;
-        for(i = 0; i < tam; i++){
-            id_versao_simulado++;
-            for(j = 0; j < versoesQuestionarios.length; j++){
-                let versao = {
-                    id_versao_simulado : id_versao_simulado,
-                    id_versao_questionario : versoesQuestionarios[j].id_versoes[i].id_questionario
-                }
+
+        for(i = 0; i < result.length; i++){
+            id++;
+            let versao = {
+                id_versao_simulado : id,
+                versao_simulado : i + 1,
+                simulado_id_simulado : id_simulado,
+                versao_questionario_id_versao_questionario : result[i].id_versao_questionario
             }
+
+            simulado.gerarVersaoSimulado(versao, function(err, res){
+                if(err){
+                    console.log(err);
+                    return err;
+                }
+                return setVersoesSimulados(id, id_simulado, questionarios, index + 1);
+            });
         }
     });
-}
-
-function getVersoesQuestionarios(versoesQuestionarios, result){
-    for(i = 0; i < result.length; i++){
-        simulado.getVersoesQuestionarios(result[i].id_questionario, function(err, res){
-            if(err){
-                return [];
-            }
-        });
-    }
 }
--- app/routes/simulado.js
+++ app/routes/simulado.js
@@ -1,6 +1,11 @@
 module.exports = function(app){
+    app.get('/criarSimulado', function(req, res){
+        var simulado = require('../controllers/simulado');
+        simulado.createSimulado(req.query, res);
+    });
+
     app.post('/simulado', function(req, res){
         var simulado = require('../controllers/simulado');
-        simulado.getQuestionarios(req.body, res);
+        simulado.gerarVersoesSimulados(req.body, res);
     });
 }
